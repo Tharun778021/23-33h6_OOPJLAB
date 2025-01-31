39c9ddabf7328e25d3b60ff75333eac9ec14a926
Tharun778021 <234g1a33h6@srit.ac.in>
2025-01-31 15:54:33 +0530

    Create Students.java

---
+++ Students.java
@@ -0,0 +1,15 @@
+class course
+{
+     void display()
+    {
+       System.out.println("course:" +"CSM");
+     }
+}
+class Students
+{
+      public static void main(String args[])
+      {
+          course obj=new course();
+           obj.display();
+       }
+}
